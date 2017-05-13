3352e9a40115c14e11e7e87b74fbdad0c3f2d782
Rahul <rahul346@gmail.com>
2017-05-13 18:25:24 +0000

    upload the default apache chnages with platform famaliy

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,10 +3,15 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
- 
 
+
+if node['platform_family'] == "rhel" 
+	pkg = "httpd"
+elsif node['platform_family'] == "debian"
+	pkg = "apache2"
+end
 package 'apache2' do
-       package_name 'httpd'
+       package_name pkg
        action [:install]
 end 
 
@@ -14,3 +19,5 @@ service 'apache2' do
 	service_name 'httpd'	
 	action [:start, :enable]
 end
+
+#include_recipe 'apache::website'
